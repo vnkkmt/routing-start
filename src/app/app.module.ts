2728bc45f450718fb9f08b2da119428f6535881f
VinayakKamath <vinayakkamath@qburst.com>
2023-02-09 14:48:05 +0530

    Setting up loading  routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
@@ -13,6 +13,11 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 
+const appRoutes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'users', component:UserComponent},
+  {path:'servers', component:ServerComponent}
+]
 
 @NgModule({
   declarations: [
@@ -27,7 +32,7 @@ import { ServersService } from './servers/servers.service';
   imports: [
     BrowserModule,
     FormsModule,
-    
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
