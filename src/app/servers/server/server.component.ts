38f53591c33383b64e707c99684a6a024dc0f30f
VinayakKamath <vinayakkamath@qburst.com>
2023-02-13 12:54:29 +0530

    preserving query params from previous page to new

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 import { ServersService } from '../servers.service';
 
@@ -13,7 +13,8 @@ export class ServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
 
   constructor(private serversService: ServersService,
-    private route:ActivatedRoute) { }
+    private route:ActivatedRoute,
+    private router:Router) { }
 
   ngOnInit() {
     this.route.params.subscribe(
@@ -22,6 +23,10 @@ export class ServerComponent implements OnInit {
       }
     )
     this.server = this.serversService.getServer(this.id);
+  }
+
+  onEdit(){
+    this.router.navigate(['edit'], {relativeTo: this.route, queryParamsHandling:'preserve'});
   }
 
 }
